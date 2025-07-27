757c04f45a38a46f3541591cd11f115cb3105b77
ton1czech <tonyasek007@gmail.com>
2025-07-27 10:35:26 +0200

    🎁NEW: map dynamic import

--- app/(homepage)/_containers/places.tsx
+++ app/(homepage)/_containers/places.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import { Border } from '@/components/border'
 import { Container } from '@/components/container'
-import { Map } from '@/components/map'
+import dynamic from 'next/dynamic'
+const Map = dynamic(
+  () => import('@/components/map').then(mod => ({ default: mod.Map })),
+  { ssr: false }
+)
 
 export const Places = () => {
   return (
