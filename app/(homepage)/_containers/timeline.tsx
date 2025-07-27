cd7bd831aed566b4345c4715402d90f158e89b8a
ton1czech <tonyasek007@gmail.com>
2025-07-27 10:31:03 +0200

    🎁NEW: timeline wip

--- app/(homepage)/_containers/timeline.tsx
+++ app/(homepage)/_containers/timeline.tsx
@@ -22,7 +22,7 @@ export const Timeline = () => {
       </h3>
       <ol
         ref={ref}
-        className='relative border-l-2 border-zinc-400 dark:border-zinc-600 space-y-20 lg:space-y-32 max-w-[80ch] mx-auto'
+        className='relative border-l-2 border-zinc-400 dark:border-zinc-600 space-y-20 lg:space-y-32 mx-auto'
       >
         <motion.div
           className='absolute -left-[3px] z-10 w-[4px] h-full bg-brand-action origin-top'
@@ -30,58 +30,26 @@ export const Timeline = () => {
         />
         <TimelineItem
           year='1748'
-          label={
-            language === 'en'
-              ? 'Embarking on Visual Foundations'
-              : 'Začínáme s vizuálním základem'
-          }
-          body={
-            language === 'en'
-              ? "In the first step, we'll focus on creating the fundamental structure and design of the website. Creativity meets functionality as we craft a visual identity that captivates and engages visitors."
-              : 'V prvním kroku se společně zaměříme na vytvoření základní struktury a designu webu. Kreativita se setká s funkcionalitou, abychom vytvořili vizuální podobu, která bude zaujímat a lákat návštěvníky.'
-          }
+          label='Nullam sapien sem'
+          body='Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Integer imperdiet lectus quis justo. Nulla pulvinar eleifend sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin in tellus sit amet nibh dignissim sagittis. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam ante. Etiam egestas wisi a erat. Duis risus. Aenean vel massa quis mauris vehicula lacinia.'
           src='/products/citron.webp'
         />
         <TimelineItem
           year='1784'
-          label={
-            language === 'en'
-              ? 'Content Shapes Form and Purpose'
-              : 'Obsah plní formu a smysl'
-          }
-          body={
-            language === 'en'
-              ? "The second step is dedicated to populating the website with content that is not only visually appealing but also conveys crucial information. Together, we'll work on texts, images, and other elements to give the content meaning and value."
-              : 'Druhý krok bude věnován naplnění obsahu, který bude nejen esteticky působivý, ale bude také sdělovat důležité informace. Společně pracujeme na textech, obrázcích a dalších prvcích, které dodají obsahu webu jeho význam a hodnotu.'
-          }
+          label='Pellentesque ipsum'
+          body='Pellentesque ipsum. Nulla est. Donec quis nibh at felis congue commodo. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Integer in sapien. Curabitur bibendum justo non orci. Phasellus et lorem id felis nonummy placerat. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Integer malesuada. Donec iaculis gravida nulla. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Nulla est. Mauris elementum mauris vitae tortor. Vestibulum fermentum tortor id mi. Cras pede libero, dapibus nec, pretium sit amet, tempor quis.'
           src='/products/citron.webp'
         />
         <TimelineItem
           year='1843'
-          label={
-            language === 'en'
-              ? 'Finetuning for a Flawless Impression'
-              : 'Ladění pro dokonalý dojem'
-          }
-          body={
-            language === 'en'
-              ? "The third step focuses on refining details and making subtle adjustments. We'll pay attention to every minor tweak to ensure the website exudes professionalism and aligns precisely with your vision."
-              : 'Ve Třetí kroku se zaměříme na detaily a jemné úpravy. Věnujeme se všem drobným úpravám, které zajistí, že web bude působit profesionálně a přesně podle vašich představ.'
-          }
+          label='Ut enim ad minim veniam'
+          body='Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Etiam posuere lacus quis dolor. Etiam egestas wisi a erat. Proin mattis lacinia justo. Etiam dictum tincidunt diam. Duis condimentum augue id magna semper rutrum. Etiam posuere lacus quis dolor. Duis condimentum augue id magna semper rutrum. Nullam dapibus fermentum ipsum. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Fusce tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.'
           src='/products/citron.webp'
         />
         <TimelineItem
           year='1894'
-          label={
-            language === 'en'
-              ? 'When Vision Becomes Reality'
-              : 'Když se vize stane skutečností'
-          }
-          body={
-            language === 'en'
-              ? "The final step is dedicated to final preparations for launching the website. We'll review all elements to ensure your website is ready for deployment and poised to welcome your visitors."
-              : 'Poslední krok je věnován finálním přípravám ke spuštění webu. Zkontrolujeme všechny prvky, abychom zajistili, že váš web je připraven k uvedení do provozu a k přivítání vašich návštěvníků.'
-          }
+          label='Sed ut perspiciatis unde'
+          body='Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Suspendisse nisl. Etiam dictum tincidunt diam. Cras elementum. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Aliquam erat volutpat. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.'
           src='/products/citron.webp'
         />
       </ol>
