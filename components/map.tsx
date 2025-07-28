90e0c6600d22f099eec774f9cf333893635732bf
ton1czech <tonyasek007@gmail.com>
2025-07-28 09:42:19 +0200

    🐛BUG FIX: ts expect error

--- components/map.tsx
+++ components/map.tsx
@@ -10,7 +10,7 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 
-// @ts-ignore
+// @ts-expect-error
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
