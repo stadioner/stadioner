74d78785054e9b076c5769cb0bb2d69a4ecfb43d
ton1czech <tonyasek007@gmail.com>
2025-08-01 11:26:17 +0200

    🎁NEW: kontakt page map

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,7 +1,17 @@
+import { Border } from '@/components/border'
+import { Container } from '@/components/container'
+
 export default function KontaktPage() {
   return (
-    <main>
-      <p>kontakt</p>
+    <main className='bg-brand-primary pt-32 pb-20'>
+      <Container>
+        <Border>
+          <iframe
+            src='https://www.google.com/maps/embed/v1/place?q=pivovar+kout+na+sumave&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8'
+            className='w-full h-[500px]'
+          />
+        </Border>
+      </Container>
     </main>
   )
 }
