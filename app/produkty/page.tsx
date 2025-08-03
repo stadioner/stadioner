e8ffe587f5acb5946508aa10a84db11074edce95
ton1czech <tonyasek007@gmail.com>
2025-08-03 18:49:29 +0200

    🐛BUG FIX: suspense

--- app/produkty/page.tsx
+++ app/produkty/page.tsx
@@ -1,11 +1,14 @@
 import { Products } from '@/containers/products'
+import { Suspense } from 'react'
 
 export default function ProduktyPage() {
   return (
     <main className='bg-brand-action pt-32 pb-40'>
-      <div className=''>
+      <Suspense
+        fallback={<div className='bg-brand-action py-8'>Loading...</div>}
+      >
         <Products hScreen />
-      </div>
+      </Suspense>
     </main>
   )
 }
