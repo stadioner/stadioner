c7dc77114ee807365c6d8ced00f53483adcd2c05
ton1czech <tonyasek007@gmail.com>
2025-08-11 11:11:34 +0200

    👌IMPROVE: smaller map legend

--- components/map-legend.tsx
+++ components/map-legend.tsx
@@ -4,17 +4,17 @@ export const MapLegend = () => {
   return (
     <div className='absolute top-5 right-5 z-[1000]'>
       <Border backgroundLight>
-        <div className='text-xl p-4'>
+        <div className=' px-2 py-1'>
           <p className='flex items-center'>
-            <img src='/map/pivovar.svg' className='size-10' />
+            <img src='/map/pivovar.svg' className='size-8' />
             Pivovar Stadioner
           </p>
           <p className='flex items-center'>
-            <img src='/map/pivoteka.svg' className='size-10' />
+            <img src='/map/pivoteka.svg' className='size-8' />
             Pivnice
           </p>
           <p className='flex items-center'>
-            <img src='/map/restaurace.svg' className='size-10' />
+            <img src='/map/restaurace.svg' className='size-8' />
             Restaurace
           </p>
         </div>
