d662b54a923a323aa8286ef35431598860f20ab4
ton1czech <tonyasek007@gmail.com>
2025-08-12 10:40:46 +0200

    👌IMPROVE: hero multilang

--- app/(homepage)/_containers/hero.tsx
+++ app/(homepage)/_containers/hero.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import { Container } from '@/components/container'
+import { useLanguage } from '@/store/use-language'
 
 export const Hero = () => {
+  const { language } = useLanguage()
+
   return (
     <section className='relative h-[86vh] w-full'>
       <img
@@ -12,7 +17,12 @@ export const Hero = () => {
         <Container className='mt-44 text-brand-action'>
           <h1 className='text-[140px] font-bold uppercase'>Stadioner</h1>
           <h3 className='text-4xl font-semibold -mt-10'>
-            Pivovar, kde ožívá šlechtický odkaz, řemeslo a chuť.
+            {language === 'cs' &&
+              'Pivovar, kde ožívá šlechtický odkaz, řemeslo a chuť.'}
+            {language === 'en' &&
+              'A brewery where aristocratic heritage, craftsmanship, and taste come to life.'}
+            {language === 'de' &&
+              'Eine Brauerei, in der das Erbe der Adelsfamilie, das Handwerk und der Geschmack wieder zum Leben erweckt werden.'}
           </h3>
           <p className='text-2xl'>est. 1736</p>
         </Container>
