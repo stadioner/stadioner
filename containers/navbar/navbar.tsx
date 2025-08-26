cb7a20d7c5bea932f6aa528dd534478aad215395
ton1czech <tonyasek007@gmail.com>
2025-08-26 16:34:15 +0200

    👌IMPROVE: nav container width

--- containers/navbar/navbar.tsx
+++ containers/navbar/navbar.tsx
@@ -9,6 +9,7 @@ import { NavItem } from '../../components/nav-item'
 import { Border } from '@/components/border'
 import { LanguageSelector } from '@/components/language-selector'
 import { Menu } from './menu'
+import { Container } from '@/components/container'
 
 export const Navbar = () => {
   const { language } = useLanguage()
@@ -19,134 +20,138 @@ export const Navbar = () => {
     <>
       <nav
         className={cn(
-          'fixed left-0 right-0 top-7 z-[1001] mx-auto md:grid items-center px-8 lg:px-10 xl:px-14 transition hidden'
+          'fixed left-0 right-0 top-7 z-[1001] md:grid items-center transition hidden'
         )}
       >
-        <Border background>
-          <div
-            className={cn(
-              'flex justify-between items-center h-full w-full px-4 bg-brand-secondary shadow-sm py-2'
-            )}
-          >
-            <div>
-              <ul className='flex items-center gap-6'>
-                <NavItem
-                  label={
-                    language === 'cs'
-                      ? 'Produkty'
-                      : language === 'en'
-                        ? 'Products'
-                        : language === 'de'
-                          ? 'Produkte'
-                          : ''
-                  }
-                  href='/produkty'
-                />
-                <NavItem
-                  label={
-                    language === 'cs'
-                      ? 'Prodejní Místa'
-                      : language === 'en'
-                        ? 'Sales Locations'
-                        : language === 'de'
-                          ? 'Verkaufsstellen'
-                          : ''
-                  }
-                  href='/prodejni-mista'
-                />
-              </ul>
-            </div>
+        <Container className='w-full'>
+          <Border background>
+            <div
+              className={cn(
+                'flex justify-between items-center h-full w-full px-4 bg-brand-secondary shadow-sm py-2'
+              )}
+            >
+              <div>
+                <ul className='flex items-center gap-6'>
+                  <NavItem
+                    label={
+                      language === 'cs'
+                        ? 'Produkty'
+                        : language === 'en'
+                          ? 'Products'
+                          : language === 'de'
+                            ? 'Produkte'
+                            : ''
+                    }
+                    href='/produkty'
+                  />
+                  <NavItem
+                    label={
+                      language === 'cs'
+                        ? 'Prodejní Místa'
+                        : language === 'en'
+                          ? 'Sales Locations'
+                          : language === 'de'
+                            ? 'Verkaufsstellen'
+                            : ''
+                    }
+                    href='/prodejni-mista'
+                  />
+                </ul>
+              </div>
 
-            <div className='absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2'>
-              <Border rounded background>
-                <Link
-                  href='/'
-                  className='transition duration-300 justify-self-center bg-brand-secondary rounded-full'
-                >
-                  <img src='/logo.svg' alt='logo' className='size-18 p-1' />
-                </Link>
-              </Border>
-            </div>
+              <div className='absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2'>
+                <Border rounded background>
+                  <Link
+                    href='/'
+                    className='transition duration-300 justify-self-center bg-brand-secondary rounded-full'
+                  >
+                    <img src='/logo.svg' alt='logo' className='size-18 p-1' />
+                  </Link>
+                </Border>
+              </div>
 
-            <div className='flex items-center'>
-              <ul className='flex items-center gap-6'>
-                <NavItem
-                  label={
-                    language === 'cs'
-                      ? 'Články'
-                      : language === 'en'
-                        ? 'Articles'
-                        : language === 'de'
-                          ? 'Artikel'
-                          : ''
-                  }
-                  href='/clanky'
-                />
-                <NavItem
-                  label={
-                    language === 'cs'
-                      ? 'Historie'
-                      : language === 'en'
-                        ? 'History'
-                        : language === 'de'
-                          ? 'Geschichte'
-                          : ''
-                  }
-                  href='/historie'
-                />
-                <NavItem
-                  label={
-                    language === 'cs'
-                      ? 'Kontakt'
-                      : language === 'en'
-                        ? 'Contact'
-                        : language === 'de'
-                          ? 'Kontakt'
-                          : ''
-                  }
-                  href='/kontakt'
-                />
-                <LanguageSelector />
-              </ul>
+              <div className='flex items-center'>
+                <ul className='flex items-center gap-6'>
+                  <NavItem
+                    label={
+                      language === 'cs'
+                        ? 'Články'
+                        : language === 'en'
+                          ? 'Articles'
+                          : language === 'de'
+                            ? 'Artikel'
+                            : ''
+                    }
+                    href='/clanky'
+                  />
+                  <NavItem
+                    label={
+                      language === 'cs'
+                        ? 'Historie'
+                        : language === 'en'
+                          ? 'History'
+                          : language === 'de'
+                            ? 'Geschichte'
+                            : ''
+                    }
+                    href='/historie'
+                  />
+                  <NavItem
+                    label={
+                      language === 'cs'
+                        ? 'Kontakt'
+                        : language === 'en'
+                          ? 'Contact'
+                          : language === 'de'
+                            ? 'Kontakt'
+                            : ''
+                    }
+                    href='/kontakt'
+                  />
+                  <LanguageSelector />
+                </ul>
+              </div>
             </div>
-          </div>
-        </Border>
+          </Border>
+        </Container>
       </nav>
 
       <nav
         className={cn(
-          'fixed left-0 right-0 top-5 z-[1001] mx-auto grid items-center px-2 transition md:hidden'
+          'fixed left-0 right-0 top-5 z-[1001] mx-auto grid items-center transition md:hidden'
         )}
       >
-        <Border background>
-          <div
-            className={cn(
-              'flex justify-between items-center h-full w-full px-4 bg-brand-secondary shadow-sm py-1'
-            )}
-          >
-            <LanguageSelector />
+        <Container className='w-full'>
+          <Border background>
+            <div
+              className={cn(
+                'flex justify-between items-center h-full w-full px-4 bg-brand-secondary shadow-sm py-1'
+              )}
+            >
+              <LanguageSelector />
 
-            <div className='absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2'>
-              <Border rounded background>
-                <Link
-                  href='/'
-                  className='transition duration-300 justify-self-center bg-brand-secondary rounded-full'
+              <div className='absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2'>
+                <Border rounded background>
+                  <Link
+                    href='/'
+                    className='transition duration-300 justify-self-center bg-brand-secondary rounded-full'
+                  >
+                    <img src='/logo.svg' alt='logo' className='size-18 p-1' />
+                  </Link>
+                </Border>
+              </div>
+
+              <div className='flex items-center'>
+                <button
+                  onClick={() => setIsOpen(true)}
+                  className='text-brand-action text-lg'
                 >
-                  <img src='/logo.svg' alt='logo' className='size-18 p-1' />
-                </Link>
-              </Border>
+                  Menu
+                </button>
+              </div>
             </div>
-
-            <div className='flex items-center'>
-              <button
-                onClick={() => setIsOpen(true)}
-                className='text-brand-action text-lg'
-              >
-                Menu
-              </button>
-            </div>
-          </div>
-        </Border>
+          </Border>
+        </Container>
       </nav>
 
       <AnimatePresence>
