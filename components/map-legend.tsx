c2d332dadfa3802f979401fb77ad053c9c2b52a5
ton1czech <tonyasek007@gmail.com>
2025-12-22 18:07:08 +0100

    👌IMPROVE: map legend

--- components/map-legend.tsx
+++ components/map-legend.tsx
@@ -9,26 +9,40 @@ export const MapLegend = () => {
   return (
     <div className='absolute top-0 right-0 z-[1000]'>
       <Border backgroundLight>
-        <div className=' px-2 py-1'>
+        <div className='px-2 py-1 text-xs'>
           <p className='flex items-center'>
-            <img src='/map/pivovar.svg' className='size-8' />
+            <img src='/map/pivovar.svg' className='size-7' />
             {language === 'cs' && 'Pivovar Stadioner'}
             {language === 'en' && 'Stadioner Brewery'}
             {language === 'de' && 'Brauerei Stadioner'}
           </p>
           <p className='flex items-center'>
-            <img src='/map/pivoteka.svg' className='size-8' />
+            <img src='/map/hospoda.svg' className='size-7' />
+
+            {language === 'cs' && 'Hospoda'}
+            {language === 'en' && 'Pub'}
+            {language === 'de' && 'Kneipe'}
+          </p>
+          <p className='flex items-center'>
+            <img src='/map/pivoteka.svg' className='size-7' />
 
             {language === 'cs' && 'Pivnice'}
             {language === 'en' && 'Beer bar'}
             {language === 'de' && 'Bierstube'}
           </p>
           <p className='flex items-center'>
-            <img src='/map/restaurace.svg' className='size-8' />
+            <img src='/map/restaurace.svg' className='size-7' />
 
             {language === 'cs' && 'Restaurace'}
             {language === 'en' && 'Restaurant'}
             {language === 'de' && 'Restaurant'}
+          </p>
+          <p className='flex items-center'>
+            <img src='/map/penzion.svg' className='size-7' />
+
+            {language === 'cs' && 'Penzion'}
+            {language === 'en' && 'Pension'}
+            {language === 'de' && 'Pension'}
           </p>
         </div>
       </Border>
