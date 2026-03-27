51ed1bb2997fcc9e956a0095716b01c1018a21a0
ton1czech <tonyasek007@gmail.com>
2026-03-27 10:22:28 +0100

    📈 analytics: map legend alts

--- components/map-legend.tsx
+++ components/map-legend.tsx
@@ -4,90 +4,85 @@ import { useLanguage } from '@/store/use-language'
 import Image from 'next/image'
 import { Border } from './border'
 
+const legendItems = [
+  {
+    src: '/map/pivovar.svg',
+    labels: {
+      cs: 'Pivovar STADIONER',
+      en: 'STADIONER Brewery',
+      de: 'Brauerei STADIONER'
+    }
+  },
+  {
+    src: '/map/hospoda.svg',
+    labels: {
+      cs: 'Hospoda',
+      en: 'Pub',
+      de: 'Kneipe'
+    }
+  },
+  {
+    src: '/map/pivoteka.svg',
+    labels: {
+      cs: 'Pivnice',
+      en: 'Beer bar',
+      de: 'Bierstube'
+    }
+  },
+  {
+    src: '/map/limo.svg',
+    labels: {
+      cs: 'Obchod',
+      en: 'Shop',
+      de: 'Geschäft'
+    }
+  },
+  {
+    src: '/map/restaurace.svg',
+    labels: {
+      cs: 'Restaurace',
+      en: 'Restaurant',
+      de: 'Restaurant'
+    }
+  },
+  {
+    src: '/map/penzion.svg',
+    labels: {
+      cs: 'Penzion',
+      en: 'Pension',
+      de: 'Pension'
+    }
+  }
+] as const
+
 export const MapLegend = () => {
   const { language } = useLanguage()
+  const activeLanguage =
+    language === 'en' || language === 'de' ? language : 'cs'
 
   return (
     <div className='absolute top-0 right-0 z-[1000]'>
       <Border backgroundLight>
         <div className='px-2 py-1 text-xs'>
-          <p className='flex items-center'>
-            <Image
-              src='/map/pivovar.svg'
-              className='size-7'
-              alt=''
-              width={28}
-              height={28}
-            />
-            {language === 'cs' && 'Pivovar STADIONER'}
-            {language === 'en' && 'STADIONER Brewery'}
-            {language === 'de' && 'Brauerei STADIONER'}
-          </p>
-          <p className='flex items-center'>
-            <Image
-              src='/map/hospoda.svg'
-              className='size-7'
-              alt=''
-              width={28}
-              height={28}
-            />
+          {legendItems.map((item) => {
+            const label = item.labels[activeLanguage]
 
-            {language === 'cs' && 'Hospoda'}
-            {language === 'en' && 'Pub'}
-            {language === 'de' && 'Kneipe'}
-          </p>
-          <p className='flex items-center'>
-            <Image
-              src='/map/pivoteka.svg'
-              className='size-7'
-              alt=''
-              width={28}
-              height={28}
-            />
-
-            {language === 'cs' && 'Pivnice'}
-            {language === 'en' && 'Beer bar'}
-            {language === 'de' && 'Bierstube'}
-          </p>
-          <p className='flex items-center'>
-            <Image
-              src='/map/limo.svg'
-              className='size-7'
-              alt=''
-              width={28}
-              height={28}
-            />
-
-            {language === 'cs' && 'Obchod'}
-            {language === 'en' && 'Shop'}
-            {language === 'de' && 'Geschäft'}
-          </p>
-          <p className='flex items-center'>
-            <Image
-              src='/map/restaurace.svg'
-              className='size-7'
-              alt=''
-              width={28}
-              height={28}
-            />
-
-            {language === 'cs' && 'Restaurace'}
-            {language === 'en' && 'Restaurant'}
-            {language === 'de' && 'Restaurant'}
-          </p>
-          <p className='flex items-center'>
-            <Image
-              src='/map/penzion.svg'
-              className='size-7'
-              alt=''
-              width={28}
-              height={28}
-            />
-
-            {language === 'cs' && 'Penzion'}
-            {language === 'en' && 'Pension'}
-            {language === 'de' && 'Pension'}
-          </p>
+            return (
+              <p
+                key={item.src}
+                className='flex items-center'
+              >
+                <Image
+                  src={item.src}
+                  className='size-7'
+                  alt={label}
+                  width={28}
+                  height={28}
+                />
+                {label}
+              </p>
+            )
+          })}
         </div>
       </Border>
     </div>
